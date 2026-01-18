3559ffe24d715451e10cf33ba896d9ff94931547
MB <m.p.bialoszewski@outlook.com>
2026-01-18 19:28:10 +0000

    Placeholder data for modal

---
+++ src/constants/data.js
@@ -0,0 +1,27 @@
+export const imagesData = [
+  { 
+    id: 1, 
+    url: 'https://images.unsplash.com/photo-1636586108931-a8b9b8796ba6?q=80&w=1736&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 
+    title: 'SPA API', 
+    author: 'Java, Spring Boot, Hibernate, H2, Maven',
+    history: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu tempor leo, nec bibendum lacus. Ut turpis arcu, rhoncus maximus dapibus ut, tempus et nunc. Suspendisse potenti. Praesent tempor leo ac felis accumsan, vel cursus arcu fermentum. Nunc imperdiet, mi vel pretium tristique, nunc mauris iaculis mauris, ut mollis odio augue sit amet eros. Vestibulum.' 
+  },
+  { 
+    id: 2, 
+    url: 'https://imgs.search.brave.com/ZC2GbymB_2ZW26hYjFltysIz6O_crSG85cBV34KW3Lo/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9ndWlk/ZXMucnVieW9ucmFp/bHMub3JnL2ltYWdl/cy9nZXR0aW5nX3N0/YXJ0ZWQvcmFpbHNf/d2VsY29tZS5wbmc', 
+    title: 'Dog Adoption Agency', 
+    author: 'Ruby on Rails/ React',
+   history: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu tempor leo, nec bibendum lacus. Ut turpis arcu, rhoncus maximus dapibus ut, tempus et nunc. Suspendisse potenti. Praesent tempor leo ac felis accumsan, vel cursus arcu fermentum. Nunc imperdiet, mi vel pretium tristique, nunc mauris iaculis mauris, ut mollis odio augue sit amet eros. Vestibulum.'   },
+  { 
+    id: 3, 
+    url: 'https://imgs.search.brave.com/slESFKV-sW17u39HrWkggg-KW00U95iJ7l3qDL5xfb8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9yZXBv/c2l0b3J5LWltYWdl/cy5naXRodWJ1c2Vy/Y29udGVudC5jb20v/NzI2MTA1MDQwLzgz/MTVlM2JmLTEzMWEt/NDBhYS1hZmI0LWI5/OTdhNGMxNjY0Yg', 
+    title: 'Jumping Rabbit Game', 
+    author: 'JavaScript, C3/D3.js',
+   history: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu tempor leo, nec bibendum lacus. Ut turpis arcu, rhoncus maximus dapibus ut, tempus et nunc. Suspendisse potenti. Praesent tempor leo ac felis accumsan, vel cursus arcu fermentum. Nunc imperdiet, mi vel pretium tristique, nunc mauris iaculis mauris, ut mollis odio augue sit amet eros. Vestibulum.'   },
+  { 
+    id: 4, 
+    url: 'https://imgs.search.brave.com/6zadYBAosPEVIwWcv5Bf0It0VBl6jlPO4EnqEjXowpM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tYXJr/ZXRwbGFjZS5jYW52/YS5jb20vRUFHblVE/amMwYTgvMS8wLzE2/MDB3L2NhbnZhLWJs/YWNrLWFuZC15ZWxs/b3ctbmVvbi1ncmFk/aWVudC1tb2Rlcm4t/Ym9sZC1zcGFya2xl/LWdlb21ldHJpYy1w/aG90b2dyYXBoeS1w/b3J0Zm9saW8tcHJl/c2VudGF0aW9uLVc3/RzFkT2FuZG1zLmpw/Zw', 
+    title: 'Portfolio Page', 
+    author: 'JavaScript, React, CSS',
+   history: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu tempor leo, nec bibendum lacus. Ut turpis arcu, rhoncus maximus dapibus ut, tempus et nunc. Suspendisse potenti. Praesent tempor leo ac felis accumsan, vel cursus arcu fermentum. Nunc imperdiet, mi vel pretium tristique, nunc mauris iaculis mauris, ut mollis odio augue sit amet eros. Vestibulum.'   }
+];
